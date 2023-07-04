5f9860361052c9dc55edd875bfb5b4a174105878
Blaise <ejikemeblaise08@gmail.com>
2023-07-04 16:24:20 +0100

    services and features

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const time = new Date().getFullYear()
 
+let i = [2, 5, 6, 8]
 
+function age(arr) {
+    return arr.reduce((acc, currentValue) => {
+        return acc + currentValue
+    }, [])
+}
+
+console.log(age(i))
